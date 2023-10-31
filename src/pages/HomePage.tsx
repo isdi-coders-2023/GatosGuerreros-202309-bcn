ea8616a805fa8b50c66b62d7b7f12a1dda81bef2
Ivan Aguilar <ivan@MacBook-Pro-de-Ivan.local>
2023-10-31 12:39:46 +0100

    Add hero image to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,12 @@
 const HomePage = (): React.ReactElement => {
   return (
     <>
+      <img
+        src="/public/images/hero_picture.webp"
+        alt="foto de la homep"
+        width={320}
+        height={652}
+      />
       <h3 className="hero__Text">
         The most curated repository of all Studio Ghibli films
       </h3>
