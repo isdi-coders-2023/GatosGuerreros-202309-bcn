2117d0b475db5f4942cc49b66b35a74a462ea783
Daniel Andreu <daniel1997ar@gmail.com>
2023-10-31 11:55:05 +0100

    Add types.ts to movie context

---
+++ src/features/movies/store/types.ts
@@ -0,0 +1,18 @@
+export interface MovieStructure {
+  id: string;
+  title: string;
+  original_title: string;
+  original_title_romanised: string;
+  image: string;
+  movie_banner: string;
+  description: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  running_time: string;
+  rt_score: string;
+}
+
+export interface MovieContextStructure {
+  movies: MovieStructure[];
+}
