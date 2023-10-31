83f39f75078ec8d7e4ae902e19ace03ad6bfcfc0
Ivan Aguilar <ivan@MacBook-Pro-de-Ivan.local>
2023-10-31 10:27:49 +0100

    Create HomePage and add to App

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+const HomePage = (): React.ReactElement => {
+  return (
+    <>
+      <h3 className="hero__Text">
+        The most curated repository of all Studio Ghibli films
+      </h3>
+      <span className="heroSymbol">↓</span>
+    </>
+  );
+};
+
+export default HomePage;
