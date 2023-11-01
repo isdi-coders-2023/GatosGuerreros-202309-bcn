a06ea969258438535640f66d4ca25814f8b98c2b
ramiro elisei <ramiroelisei@gmail.com>
2023-11-01 18:40:05 +0100

    Add mainTheme rename to main import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { ThemeProvider } from "styled-components";
-import { mainTheme } from "./styles/MainTheme";
+import { mainTheme } from "./styles/mainTheme";
 import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter } from "react-router-dom";
 import MovieProviderWrapper from "./features/movies/store/MoviesProviderWrapper";
