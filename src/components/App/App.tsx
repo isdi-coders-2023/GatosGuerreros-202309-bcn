71a212fefc95965a9fc111d61302108a159935a8
ramiro elisei <ramiroelisei@gmail.com>
2023-11-01 22:33:43 +0100

    Add Header component to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
 import MoviesList from "../MoviesList/MoviesList";
+import Header from "../Header/Header";
 
 const App = (): React.ReactElement => {
+  const path = window.location.pathname;
   return (
     <>
+      <Header path={path} />
       <MoviesList></MoviesList>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
