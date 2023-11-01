3b75982dd9f901114f6263425b9aa045c56a1bd1
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-01 19:56:40 +0100

    Add loadItems and MovieList in HomePage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
-import MoviesList from "../MoviesList/MoviesList";
 
 const App = (): React.ReactElement => {
   return (
     <>
-      <MoviesList></MoviesList>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<HomePage />} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,33 @@
+import { useContext, useEffect } from "react";
+import MoviesContext from "../features/movies/store/MoviesContext";
+import MoviesList from "../components/MoviesList/MoviesList";
+
 const HomePage = (): React.ReactElement => {
+  const { loadMovies } = useContext(MoviesContext);
+  useEffect(() => {
+    loadMovies([
+      {
+        description: "",
+        director: "Hayao Miyazaki",
+        id: 1,
+        title: "My Neighbor Totoro",
+        release_date: "",
+        running_time: "",
+        movie_banner:
+          "https://image.tmdb.org/t/p/original/etqr6fOOCXQOgwrQXaKwenTSuzx.jpg",
+      },
+      {
+        description: "",
+        director: "Isao Takahata",
+        id: 2,
+        title: "Only Yesterday",
+        release_date: "",
+        running_time: "",
+        movie_banner:
+          "https://image.tmdb.org/t/p/w533_and_h300_bestv2/isCrlWWI4JrdLKAUAwFb5cjAsH4.jpg",
+      },
+    ]);
+  }, [loadMovies]);
   return (
     <>
       <img
@@ -7,10 +36,11 @@ const HomePage = (): React.ReactElement => {
         width={320}
         height={652}
       />
-      <h3 className="hero__Text">
+      <h2 className="hero__Text">
         The most curated repository of all Studio Ghibli films
-      </h3>
+      </h2>
       <span className="heroSymbol">↓</span>
+      <MoviesList></MoviesList>
     </>
   );
 };
