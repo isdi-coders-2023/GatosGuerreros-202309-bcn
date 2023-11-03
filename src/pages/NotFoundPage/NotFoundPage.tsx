20919438cdd2f25685de1903865b00c81dbfb484
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-03 20:42:06 +0100

    Add NotFoundPage with styles

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom";
+import NotFoundPageStyled from "./NotFoundPageStyled";
 
 const NotFoundPage = (): React.ReactElement => {
   return (
-    <>
+    <NotFoundPageStyled>
       <h2 className="not-found__alert">Oops!</h2>
-      <span className="not-found__span">It's just a 404 Error!</span>
-      <span className="not-found__feedback">
-        What you’re looking for may have been misplaced in Long Term Memory.
-      </span>
-      <NavLink to="/home" className="not-found__button">
-        Go Home
-      </NavLink>
-    </>
+      <div className="not-found__container">
+        <span className="not-found__error">It's just a 404 Error!</span>
+        <span className="not-found__feedback">
+          What you’re looking for may have been misplaced in Long Term Memory.
+        </span>
+        <NavLink to="/home" className="not-found__button">
+          Go Home
+        </NavLink>
+      </div>
+    </NotFoundPageStyled>
   );
 };
 
